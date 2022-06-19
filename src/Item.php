2da1ee3a43bc509dc3a3943870c6bd14128cdc51
guitarbien <guitarbien@gmail.com>
2022-06-20 00:46:00 +0800

    init

---
+++ src/Item.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace GildedRose;
+
+final class Item
+{
+    public function __construct(public string $name, public int $sellIn, public int $quality)
+    {
+    }
+
+    public function __toString(): string
+    {
+        return "{$this->name}, {$this->sellIn}, {$this->quality}";
+    }
+}
