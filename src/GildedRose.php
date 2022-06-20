dc29ca9c17a9ed7686c2fe7d7dfaf95a482b23a4
guitarbien <guitarbien@gmail.com>
2022-06-21 01:25:48 +0800

    refactor: 整理程式，測試全過

--- src/GildedRose.php
+++ src/GildedRose.php
@@ -92,18 +92,13 @@ final class GildedRose
     private function agedBrieTick(Item $item): void
     {
         $item->sellIn -= 1;
-        if ($item->quality === 50) {
+        if ($item->quality >= 50) {
             return;
         }
 
-        if ($item->sellIn < 0) {
-            $item->quality += 2;
-        } else {
+        $item->quality += 1;
+        if ($item->sellIn <= 0 && $item->quality < 50) {
             $item->quality += 1;
-        }
-
-        if ($item->quality > 50) {
-            $item->quality = 50;
         }
     }
 }
