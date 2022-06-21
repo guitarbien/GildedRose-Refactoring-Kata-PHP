0c689c6671d1667a287dd36b93a9c6b6cd7b0a98
guitarbien <guitarbien@gmail.com>
2022-06-21 19:07:09 +0800

    refactor: 抽出重複的部份，測試全過

--- src/GildedRose.php
+++ src/GildedRose.php
@@ -4,39 +4,27 @@ declare(strict_types=1);
 
 namespace GildedRose;
 
+use RuntimeException;
+
 final class GildedRose
 {
-    /**
-     * @var Item[]
-     */
-    private $items;
-
-    public function __construct(array $items)
+    public function __construct(private array $items)
     {
-        $this->items = $items;
     }
 
     public function updateQuality(): void
     {
         foreach ($this->items as $item) {
-            switch ($item->name) {
-                case 'normal':
-                    $normalItem = new Normal($item);
-                    $normalItem->tick();
-                    return;
-                case 'Aged Brie':
-                    $agedBrieItem = new AgedBrie($item);
-                    $agedBrieItem->tick();
-                    return;
-                case 'Sulfuras, Hand of Ragnaros':
-                    $sulfurasItem = new Sulfuras($item);
-                    $sulfurasItem->tick();
-                    return;
-                case 'Backstage passes to a TAFKAL80ETC concert':
-                    $backstageItem = new Backstage($item);
-                    $backstageItem->tick();
-                    return;
-            }
+            $className = match ($item->name) {
+                'normal' => Normal::class,
+                'Aged Brie' => AgedBrie::class,
+                'Sulfuras, Hand of Ragnaros' => Sulfuras::class,
+                'Backstage passes to a TAFKAL80ETC concert' => Backstage::class,
+                default => throw new RuntimeException('Unknown item type'),
+            };
+
+            $newItem = new $className($item);
+            $newItem->tick();
         }
     }
 }
